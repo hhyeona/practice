1bfba8543790b128e8581d492e4b7cd075b7a88a
Hyeona Jeong <122460802+hhyeona@users.noreply.github.com>
2024-01-10 22:45:17 +0900

    [Bronze I] Title: 팰린드롬수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
@@ -3,12 +3,14 @@
 0 은 앞에 올 수 없음.
 1 이상 99999 이하의 정수가 주어짐.
 마지막 줄에는 0이 포함.
+
+why? import sys 쓰면 안되지?
 '''
-#import sys
-#input = sys.stdin.readline
+import sys
 
 while True:
-    lst = input()
+    input = sys.stdin.readline().rstrip()
+    lst = input
     if lst == '0':
         break
 
