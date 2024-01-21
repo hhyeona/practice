d02283bb4624973245b8143b871b462bed5af0f4
Hyeona Jeong <122460802+hhyeona@users.noreply.github.com>
2024-01-21 13:50:59 +0900

    [Silver III] Title: 바이러스, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,25 +1,41 @@
-def dfs(start):
-    global cnt
-    visited[start] = 1
-    for i in graph[start]:
-        if visited[i] == 0:
-            dfs(i)
-            cnt += 1
+'''
+바이러스
+바이러스에 걸리게 되는 컴퓨터의 수 출력.
+컴퓨터 수 100이하.
+노트(간선의수) 주어짐.
+연결되어있는 컴퓨터 번호의 쌍이 주어짐.
+7
+6
+1 2
+2 3
+1 5
+5 2
+5 6
+4 7
+1번이 걸렸을 때, 1번을 통해 감염되는 컴퓨터의 총 수 출력.
+4
 
+DFS로 품.
+깊이 우선 탐색.
+'''
+import sys
+input = sys.stdin.readline
 
-n = int(input())
-m = int(input())
-graph = [[] * n for _ in range(n + 1)]
-for _ in range(m):
-    a, b = map(int, input().split())
-    graph[a].append(b)
-    graph[b].append(a)
+node = int(input())
+line = int(input())
+graph = [[] for i in range(node+1)]  # 그래프 초기화 (인접 리스트 만들 것임)
+visited = [0]*(node+1)               # 방문 표시 따로.
 
-cnt = 0
-visited = [0] * (n + 1)
+def dfs(line):              # 그래프 끼리 연결된 부분 방문 표시 하나씩 해줌.
+    visited[line] = 1
+    for nx in graph[line]:   # 현재 노드와 연결 된 다른 노드를 재귀적으로 방문.
+        if visited[nx] == 0:
+            dfs(nx)
 
+for i in range(line):
+    a,b = map(int,input().split())   # 입력값들을 서로 연결시켜줌.(리스트로 변환)
+    graph[a] += [b]
+    graph[b] += [a]
 
-
-
-dfs(1)
-print(cnt)
+dfs(1) # 1과 연결 된 점.
+print(sum(visited)-1) # 1번 컴퓨터 제외.
