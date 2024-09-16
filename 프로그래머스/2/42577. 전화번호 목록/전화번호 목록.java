1de07edad2361d83f0cc7407d5c65a1d99dde666
정현아 <122460802+hhyeona@users.noreply.github.com>
2024-09-16 20:58:16 +0900

    [level 2] Title: 전화번호 목록, Time: 277.39 ms, Memory: 245 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,13 +1,21 @@
 import java.util.*;
 class Solution {
     public boolean solution(String[] phone_book) {
-        // boolean answer = true;
-        Arrays.sort(phone_book);
-                
-        for (int i=0; i < phone_book.length -1; i++ ){
-            if (phone_book[i+1].startsWith(phone_book[i]))
-                return false;
+        HashMap<String,Integer> map = new HashMap<>();
+        
+        for (String phone_number : phone_book){
+            map.put(phone_number,1);
         }
+        
+        // System.out.println(map.toString());
+        
+        for (String phone_number : phone_book){
+            for (int i = 1; i<phone_number.length(); i++ ){
+                if (map.containsKey(phone_number.substring(0,i)))
+                    return false;
+            }
+        }
+        
         
         return true;
     }
