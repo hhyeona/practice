10dc725d40af50c346cb114c46ed6d441d88a1d1
정현아 <122460802+hhyeona@users.noreply.github.com>
2024-09-18 15:31:55 +0900

    [level 2] Title: 기능개발, Time: 7.18 ms, Memory: 80.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        List<Integer> day = new ArrayList<>();
+        List<Integer> answer = new ArrayList<>();
+        
+        for (int i=0; i<progresses.length; i++){
+            int tm = (int)Math.ceil((100 - progresses[i]) / (double) speeds[i]) ;
+            day.add(tm);
+        }
+        // System.out.println(day);
+      
+        int tmm = day.get(0);
+        int sum = 1;
+        for (int j = 1; j < day.size(); j++){
+            if (tmm < day.get(j)){
+                answer.add(sum);
+                sum = 1;
+                tmm = day.get(j);
+            } else {
+                sum += 1;
+            }
+            
+        }
+        answer.add(sum);
+        
+        
+        return answer.stream().mapToInt(i->i).toArray();
+    }
+}
