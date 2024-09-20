7200d43a72977bea56461bb489c74dbdf7d447eb
정현아 <122460802+hhyeona@users.noreply.github.com>
2024-09-20 19:15:04 +0900

    [level 2] Title: 가장 큰 수, Time: 186.15 ms, Memory: 136 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -2,22 +2,21 @@ import java.util.*;
 class Solution {
     public String solution(int[] numbers) {
         String[] arr = new String[numbers.length];
-    
         int index = 0;
-        for (int number : numbers){
-            arr[index++] = String.valueOf(number);
+        
+        for (int num : numbers){
+            arr[index++] = String.valueOf(num);
         }
         
-        Arrays.sort(arr,(s1,s2) -> (s2+s1).compareTo(s1+s2));
+        Arrays.sort(arr, (s1,s2) -> (s2+s1).compareTo(s1+s2));
         
-        // System.out.println(Arrays.toString(arr));
-        if (arr[0].equals("0")){
+        if(arr[0].equals("0")){
             return "0";
         }
         
-        
         StringBuilder answer = new StringBuilder();
-        for (String item : arr){
+        
+        for(String item : arr){
             answer.append(item);
         }
         
