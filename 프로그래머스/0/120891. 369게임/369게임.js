782b0c95ea48b368ec808b5db3fff4706176762c
정현아 <122460802+hhyeona@users.noreply.github.com>
2024-10-30 13:29:08 +0900

    [level 0] Title: 369게임, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120891. 369게임/369게임.js
+++ 프로그래머스/0/120891. 369게임/369게임.js
@@ -1,5 +1,11 @@
 function solution(order) {
 //     nullish 연산자 null 또는 undefined일때, 오른쪽 아니라면 왼쪽.
-    let value = order.toString().match(/[369]/g) ?? []
-    return value.length;
+    // let value = order.toString().match(/[369]/g) ?? []
+    // return value.length;
+    
+//     filter 
+    // return order.toString().split('').filter(v=>v==='3'|| v==='6'||v==='9' ? true : false).length
+//     Set
+    const s = new Set('369')
+    return order.toString().split('').filter(v=>s.has(v)).length
 }
