47c5e9215d14b2f569ab261709c6e303a337623c
정현아 <122460802+hhyeona@users.noreply.github.com>
2024-12-17 23:34:47 +0900

    [Bronze I] Title: 평균, Time: 204 ms, Memory: 18736 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,7 +1,8 @@
 import java.util.*;
 
-public class  Main {
+public class Main {
     public static void main(String[] args){
+        /*
 //        입력값 받기
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -20,6 +21,23 @@ public class  Main {
         }
         // 평균내기.
         float ans = sm/n;
+        System.out.println(ans);
+
+         */
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] lst = new int[n];
+        for(int i = 0; i< n; i++){
+            lst[i] = sc.nextInt();
+        }
+        Arrays.sort(lst);
+        int mx = lst[n-1];
+        int sm = 0;
+//        (a+b+c)*100/M/n 으로 공식 단순화.
+        for(int num:lst){
+            sm += num;
+        }
+        float ans = (float) sm*100/mx/n;
         System.out.println(ans);
     }
 }
